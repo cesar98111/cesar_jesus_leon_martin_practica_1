5e5c246d9f1f48ee05ef18389afbe6db3d03cf6d
Cesar Jesus Leon Martin <cesilius98@gmail.com>
2022-10-04 22:57:20 +0200

    Feature: se ha conseguido meter la imagen en el Header

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>victor puto</Text>
+      <View style={styles.Header}>
+        <Image source={require('./assets/miCara.jpg')}></Image>
+      </View>
+      <View style={styles.body}>
+
+      </View>
+
       
     </View>
   );
@@ -12,9 +18,23 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
+    alignItems:'flex-start',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Header:{
+    backgroundColor:'grey',
+    flex:1,
+    width: '100%',
+    height: 30
+    
+  },
+  body:{
+    flex: 3
+  }
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 #### esto es un archivo markdown
+
+* https://code.tutsplus.com/es/tutorials/understanding-the-image-component-in-react-native--cms-35877
