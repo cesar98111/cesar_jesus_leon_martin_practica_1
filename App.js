225bee97d455f6814587b072749d6bfdc78f72ba
Cesar Jesus Leon Martin <cesilius98@gmail.com>
2022-10-04 23:06:07 +0200

    Fix: arreglado con el escalado y posicionamiento de la imagen

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.Header}>
-        <Image source={require('./assets/miCara.jpg')}></Image>
+        <Image 
+        source={require('./assets/miCara.jpg')}
+        style={styles.Image}></Image>
       </View>
       <View style={styles.body}>
 
@@ -30,9 +32,18 @@ const styles = StyleSheet.create({
   Header:{
     backgroundColor:'grey',
     flex:1,
+    justifyContent:'center',
+    
     width: '100%',
     height: 30
     
+  },
+  Image:{
+      marginLeft:30,
+      width:100,
+      height:100,
+      borderRadius:50
+      
   },
   body:{
     flex: 3
